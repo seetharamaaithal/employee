421608e583876661aec972bedc2accc9a8054d0c
Seetharama <seetharama1989@gmail.com>
2017-05-16 16:05:58 -0400

    Adding the files.

---
+++ public/js/time-carousel.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+      $('.your-class').slick({
+        slidesToShow: 5,
+		  slidesToScroll: 1,
+		  autoplay: false,
+		  autoplaySpeed: 2000,
+		  arrows:true,
+      });
+    });
